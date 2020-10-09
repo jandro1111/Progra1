79730300beeb0f35d26611682e69e5bebd3802ee
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-10-09 08:52:59 -0300

    ya lo termine

--- main.c
+++ main.c
@@ -14,16 +14,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"input.h"
+#define NXDAT 2
 /*
- * 
+ * \
  */
 int main(int argc, char* argv[]) {
     int data[]={0,0};  //en el primer lugar guardo la cantidad de opciones
     int *dat;
+    int i, b;
     dat=data;
     input(dat,argv,argc);
-    printf("%d %d", data[0], data[1]);
-   
+    for(i=1, b=1;i<=data[0];++i){//imprimo primero las opciones
+        printf("Opcion %d: Clave: %s Valor: %s \n", i, argv[b], argv[b+1]);
+        b=b+NXDAT;
+    }//b ya queda apuntando a los parametros, x eso no la inicializo
+    for(i=1;i<=data[1];++i,++b){//imprimo los parametros
+        printf("Parametro %d: %s \n", i, argv[b]);
+    }
     return (EXIT_SUCCESS);
 }
 
