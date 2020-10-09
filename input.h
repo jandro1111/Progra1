0a3442767b52617ea796a0e5b35aa2a19d5d23f0
jandro1111 <jandro1111@jandro1111-VirtualBox>
2020-10-08 21:01:55 -0300

    input.h

---
+++ input.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   input.h
+ * Author: jandro1111
+ *
+ * Created on 8 de octubre de 2020, 18:44
+ */
+
+#ifndef INPUT_H
+#define INPUT_H
+void input(int *, char *[],int);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INPUT_H */
+
